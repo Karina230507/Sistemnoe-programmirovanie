c212ec0dfd3f8d5f44a3f008365718e8992665b3
Karina230507 <karinaevs2007@gmail.com>
2025-11-12 00:49:31 +0300

    Add files via upload

---
+++ zadacha-5/Main.java
@@ -0,0 +1,10 @@
+
+
+/**
+ * Launcher - запускает демонстрацию работы с общим ресурсом
+ */
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        new InterferenceExample().example();
+    }
+}
